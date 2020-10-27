80964f51edab277a56e9365fda0e34745808e006
Raj Kumar Shahu <rajkumarshahu@gmail.com>
2020-10-27 19:48:25 -0400

    Initial Commit

---
+++ Simple-Shopping-List-App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Simple-Shopping-List-App
+//
+//  Created by Raj Kumar Shahu on 2020-10-27.
+//  Copyright © 2020 Centennial College. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
